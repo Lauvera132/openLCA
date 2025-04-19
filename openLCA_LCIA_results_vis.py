de8ebeb1b8741495b7e384e18bc642b85fce4418
Laura Rivera <lr28372@my.utexas.edu>
2025-04-19 18:26:02 -0500

    print target variables, clean up df

--- openLCA_LCIA_results_vis.py
+++ openLCA_LCIA_results_vis.py
@@ -20,8 +20,9 @@ df_transposed = df.transpose()
 df_transposed.reset_index(drop=True, inplace=True)
 
 # Find the column index containing the specified string in row index 1
-target_string = "climate change - global warming potential (GWP100) (Nitrous Oxide, Methane, Carbon Dioxide)"
-column_index = df_transposed.columns[df_transposed.iloc[1] == target_string].tolist()
+impact_category_name = "climate change - global warming potential (GWP100) (Nitrous Oxide, Methane, Carbon Dioxide)"
+print(f"Impact category name: {impact_category_name}")
+column_index = df_transposed.columns[df_transposed.iloc[1] == impact_category_name].tolist()
 
 # If the column is found, retrieve its index; otherwise, return None
 column_index = column_index[0] if column_index else None
@@ -29,4 +30,15 @@ column_index = column_index[0] if column_index else None
 # Create a copy of df_transposed with only the specified columns
 columns_to_keep = [0, 1, 2, column_index]
 filtered_df = df_transposed.iloc[:, columns_to_keep]
+
+impact_category_uuid = filtered_df.iloc[0, 3]
+impact_category_reference_unit = filtered_df.iloc[2, 3]
+print(f"Impact category UUID: {impact_category_uuid}")
+print(f"Impact category reference unit: {impact_category_reference_unit}")
+
+# Delete rows 0-3 from the filtered DataFrame
+filtered_df = filtered_df.iloc[4:].reset_index(drop=True)
+
+# Rename the columns of the filtered DataFrame
+filtered_df.columns = ['Process UUID', 'Process', 'Location', 'GWP100_Impact_kg_CO2eq']
 
