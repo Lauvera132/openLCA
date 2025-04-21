4810db7d412e893903f20a23caffb5884ee8caeb
Laura Rivera <lr28372@my.utexas.edu>
2025-04-21 13:11:49 -0500

    added additional instructions for clarification and started jupiter notebook for simplicity

--- openLCA_process_df_compile.py
+++ openLCA_process_df_compile.py
@@ -46,3 +46,15 @@ print(processes_df.head())
 # Save the DataFrame to a CSV file
 output_file = r'C:\Users\laura\OneDrive\Documents\UT_graduate\WEG_graduate_research\Hydrogen_fugitive_emissions_natural_h2\openLCA_ecoinvent\openLCA\database_process_summary.csv'
 processes_df.to_csv(output_file, index=False)
+print(f"DataFrame saved to {output_file} CSV successfully.")
+
+# Save the DataFrame to an Excel file and rename the file
+excel_file = r'C:\Users\laura\OneDrive\Documents\UT_graduate\WEG_graduate_research\Hydrogen_fugitive_emissions_natural_h2\openLCA_ecoinvent\openLCA\database_process_summary_added.xlsx'
+print(f"Saving DataFrame to Excel file: {excel_file}")
+processes_df.to_excel(excel_file, index=False)
+print(f"DataFrame saved to {excel_file} successfully.")
+print("Please open the Excel file and review the 'ISIC_Category' column for any blank entries.")
+print("If blank entries are found, refer to the 'International Standard Industrial Classification of All Economic Activities (ISIC), Rev. 4' to assign an appropriate ISIC Category for the corresponding unit process.")
+print("The 'ISIC_Category' uses a four-level hierarchical structure - Section (letter), Division (2 digits), Group (3 digits), and Class (4 digits).")
+print("The 'ISIC_Category' must be entered as a string with the format 'Section/Division/Group/Class'.")
+print("Please save the Excel file after making any changes.")
