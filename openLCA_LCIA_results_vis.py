688cfc2a9251fd5f0f0682fb44dcd59221934b08
Laura Rivera <lr28372@my.utexas.edu>
2025-04-21 13:39:44 -0500

    updated naming for clarity

--- openLCA_LCIA_results_vis.py
+++ openLCA_LCIA_results_vis.py
@@ -1,15 +1,16 @@
 # Laura Rivera
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-product_system = 'Hydrogen_gas__at_processing__production_mixture__to_consumer__kg___US__85__mol_H2____H2_Average___CH4_Capture'
+product_system = 'GeoH2 (85% mol H2; H2 Average Fugitive; CH4 Capture)'
 
 # Load the Excel file and specify the worksheet name
-file_path = r'C:\Users\laura\OneDrive\Documents\UT_graduate\WEG_graduate_research\Hydrogen_fugitive_emissions_natural_h2\openLCA_ecoinvent\Hydrogen_gas__at_processing__production_mixture__to_consumer__kg___US__85__mol_H2____H2_Average___CH4_Capture.xlsx'
+lca_results_file_path = r'C:\Users\laura\OneDrive\Documents\UT_graduate\WEG_graduate_research\Hydrogen_fugitive_emissions_natural_h2\openLCA_ecoinvent\Hydrogen_gas__at_processing__production_mixture__to_consumer__kg___US__85__mol_H2____H2_Average___CH4_Capture.xlsx'
 sheet_name = 'Direct impact contributions'
 
 # Read the specified worksheet into a DataFrame
-df = pd.read_excel(file_path, sheet_name=sheet_name)
+df = pd.read_excel(lca_results_file_path, sheet_name=sheet_name)
 
 # Drop the first column if it contains only empty values
 if df.iloc[:, 0].isnull().all():
@@ -98,7 +99,6 @@ print(top_5_processes)
 print("Top 10 processes with the largest GWP100 impact:")
 print(top_10_processes)
 
-import matplotlib.pyplot as plt
 # Group the data by Process_Name and sum the GWP100_Impact_kg_CO2eq for each process
 grouped_data = merged_df.groupby('Process_Name')['GWP100_Impact_kg_CO2eq'].sum()
 
